df19cc520793839b53a4d22ddd219a7df05ed8eb
alex <drptau@gmail.com>
2018-08-07 12:45:09 -0400

    update

--- backend/read.js
+++ backend/read.js
@@ -13,24 +13,46 @@ console.log(path);
 var csvPath = jsonContents.dataResources[1].resPath;
 console.log(csvPath);
 
-var col1 = jsonContents.dataResources[1].columns[0].colName;
+var col1 = jsonContents.dataResources[1].columns[0]["colIndex"];
+console.log("col1", typeof col1);
 
-console.log(col1);
+var col2 = jsonContents.dataResources[1].columns[1]["colIndex"];
+console.log("col2", col2);
 
-var col2 = jsonContents.dataResources[1].columns[1].colName;
-console.log(col2);
+var role = jsonContents.dataResources[1].columns[0]["role"];
+console.log(role.includes("index"));
 
 const csvFilePath = csvPath;
-
-csv()
+console.log("==========");
+csv({ noheader: false })
   .fromFile(csvFilePath)
-  .then(jsonObj => {
-    console.log(jsonObj[0][col1]);
-    console.log(jsonObj[0][col2]);
-    // jsonObj.forEach(obj => {
-    //   console.log(obj);
-    // });
+  .on("csv", row => {
+    console.log(row);
+    console.log(row[0]);
+    console.log(row[1]);
+  })
+  .on("done", error => {
+    console.log("end");
   });
+
+// csv()
+//   .fromFile(csvFilePath)
+//   .then(jsonObjs => {
+//     jsonObjs.forEach(jsonObj => {
+//       console.log(parseInt(jsonObj[col1]));
+//       console.log(jsonObj[col2]);
+//     });
+//   });
+
+// jsonObjs => {
+// jsonObjs.forEach(obj=>{
+//   console.log(parseInt(jsonObj[0][col1]));
+//   console.logjsonObj[0][col2]);
+// })
+
+// jsonObj.forEach(obj => {
+//   console.log(obj);
+// });
 
 // Async / await usage
 // const jsonArray = csv().fromFile(csvFilePath);
